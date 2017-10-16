149915941edab83838787b48827e279c3f280d8d
mwerumuchai <mwerumuchai@gmail.com>
2017-10-16 18:07:50 +0300

    ABmacros
    mACROS

---
+++ app/movie_test.py
@@ -0,0 +1,29 @@
+import unittest
+from models import movie
+Movie = movie.Movie
+
+class MovieTest(unittest.TestCase):
+    '''
+    Test class to test the behavior of the movie class
+    '''
+
+    def setUp(self):
+        '''
+        setUp method that will run before every Test
+        '''
+
+        self.new_movie = Movie(1234, "Python must be crazy", "A thrilling new python series", "/khsjha27hbs", 8.5, 129993)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_movie, Movie))
+
+    def test_init(self):
+        self.assertEqual(self.new_movie.id, 1234)
+        self.assertEqual(self.new_movie.title, "Python must be crazy")
+        self.assertEqual(self.new_movie.overview, "A thrilling new python series")
+        self.assertEqual(self.new_movie.image, "https://image.tmdb.org/t/p/w500"+"/khsjha27hbs")
+        self.assertEqual(self.new_movie.vote_average, 8.5)
+        self.assertEqual(self.new_movie.vote_count, 129993)
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
---
+++ app/views.py
@@ -0,0 +1,24 @@
+from flask import render_template
+from app import app
+from .request import get_movies
+# views
+@app.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    #getting popular movie
+    popular_movies = get_movies('popular')
+    upcoming_movie = get_movies('upcoming')
+    now_showing_movie = get_movies('now_playing')
+    title = "Home - Welcome to the Best Online Movie Review Website"
+    return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing =now_showing_movie)
+
+@app.route('/movie/<int:movie_id>')
+def movie(movie_id):
+    '''
+    View root page function that returns the index page and its data
+    '''
+    title = f"You are watching movie { movie_id }"
+    return render_template('movie.html', title = title)
