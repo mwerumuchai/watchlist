e226a1b1c57895a7974143ecde8a06ed7fbb587a
mwerumuchai <mwerumuchai@gmail.com>
2017-10-18 19:07:12 +0300

    Deploying to heroku

--- manage.py
+++ manage.py
@@ -8,6 +8,17 @@ app = create_app('development')
 manager = Manager(app)
 manager.add_command('server', Server)
 
+# Running Unittest
+@manager.command
+def test():
+    '''
+    Run the Unittest
+    '''
+    import unittest
+    tests = unittest.TestLoader().discover('test')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
 
 if __name__ == '__main__':
     manager.run()
---
+++ start.sh
@@ -0,0 +1,4 @@
+export MOVIE_API_KEY=39dc5ce4e08f79718bccddb1418a2a68
+export SECRET_KEY=Flask WTF Secret Key
+
+python3.6 manage.py server
